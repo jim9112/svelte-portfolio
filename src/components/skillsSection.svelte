bad35020394dbf9277f1ae13d5a71b57a4a36b18
jim9112 <jim9112@gmail.com>
2021-06-14 14:05:14 -0400

    add skills icons

---
+++ src/components/skillsSection.svelte
@@ -0,0 +1,17 @@
+<div>
+    <img src="src/logos/css3.svg" alt="CSS3">
+    <img src="src/logos/git-icon.svg" alt="Git">
+    <img src="src/logos/html-5.svg" alt="HTML5">
+    <img src="src/logos/javascript.svg" alt="JavaScript">
+    <img src="src/logos/mongodb.svg" alt="MongoDB">
+    <img src="src/logos/nodejs-1.svg" alt="NodeJS">
+    <img src="src/logos/react-2.svg" alt="React">
+    <img src="src/logos/typescript.svg" alt="TypeScript">
+</div>
+
+<style>
+    img {
+        width: 200px;
+        height: 200px;
+    }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,12 +2,13 @@
     import Header from '../components/header.svelte'
     import Hero from '../components/hero.svelte'
     import SectionContainer from '../components/sectionContainer.svelte'
+    import SkillsSection from '../components/skillsSection.svelte'
 </script>
 
 
 <Header />
 <Hero />
-<SectionContainer title={"Skills"}><p>test</p></SectionContainer>
+<SectionContainer title={"Skills"}><SkillsSection /></SectionContainer>
 <SectionContainer title={"My Portfolio"}><p>test</p></SectionContainer>
 <SectionContainer title={"About Me"}><p>test</p></SectionContainer>
 <h1>Welcome to SvelteKit</h1>
