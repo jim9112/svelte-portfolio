3ea941d6cac032f3d84100e91099a2d363ac009f
jim9112 <jim9112@gmail.com>
2021-06-15 13:46:59 -0400

    start portfolio cards

---
+++ src/components/portfolio.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+import { loop_guard } from 'svelte/internal';
+
+    import PortfolioCard from './portfolioCard.svelte'
+    const portfolioList = [
+        'one', 'two', 'three'
+    ]
+</script>
+
+
+<div>
+    
+    {#each portfolioList as portfolio}
+    <PortfolioCard />
+{/each}
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-    import Header from '../components/header.svelte'
+    import Portfolio from '../components/portfolio.svelte'
+import Header from '../components/header.svelte'
     import Hero from '../components/hero.svelte'
     import SectionContainer from '../components/sectionContainer.svelte'
     import SkillsSection from '../components/skillsSection.svelte'
@@ -9,7 +10,7 @@
 <Header />
 <Hero />
 <SectionContainer title={"Skills"}><SkillsSection /></SectionContainer>
-<SectionContainer title={"My Portfolio"}><p>test</p></SectionContainer>
+<SectionContainer title={"My Portfolio"}><Portfolio /></SectionContainer>
 <SectionContainer title={"About Me"}><p>test</p></SectionContainer>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
